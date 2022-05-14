c855eca23fb23eda4d417865ce26f5e64c715fa5
Arkadiusz4 <arekmika4@gmail.com>
2022-05-14 16:17:07 +0200

    commit

--- lib/screens/team_screen.dart
+++ lib/screens/team_screen.dart
@@ -12,56 +12,86 @@ class TeamScreen extends StatefulWidget {
 }
 
 class _TeamScreenState extends State<TeamScreen> {
-  final List<String> _team = [];
+  // final List<TeamResponse> _team = [];
 
-  Future<List<TeamResponse>> getTeam() async {
-    //https://apiv2.allsportsapi.com/basketball/?met=Teams&APIkey=048b6af902a7611b4fd95f5ea44a3cf77c371dc49b91f24099f217329c76ce05&leagueId=766
+  var team;
 
-    final queryParametrs = {
-      'met': 'Teams',
-      'ApiKey':
-          '048b6af902a7611b4fd95f5ea44a3cf77c371dc49b91f24099f217329c76ce05',
-      'leagueId': '766'
-    };
+  Future<Result> getTeam() async {
+    final response = await http.get(Uri.parse(
+        'https://apiv2.allsportsapi.com/basketball/?met=Teams&APIkey=048b6af902a7611b4fd95f5ea44a3cf77c371dc49b91f24099f217329c76ce05&leagueId=766'));
+    var data = jsonDecode(response.body.toString());
 
-    final uri =
-        Uri.https('apiv2.allsportsapi.com', '/basketball/?', queryParametrs);
-
-    final teamResponse = await http.get(uri);
-
-    List<TeamResponse> teamList = [];
-
-    if (teamResponse.body == 200) {
-      var teamjson = json.decode(teamResponse.body);
-
-      for (var jsondata in teamjson) {
-        teamList.add(TeamResponse.fromJson(jsondata));
-      }
+    if (response.statusCode == 200) {
+      return Result.fromJson(data);
+    } else {
+      return Result.fromJson(data);
     }
 
-    return teamList;
+    //https://apiv2.allsportsapi.com/basketball/?met=Teams&APIkey=048b6af902a7611b4fd95f5ea44a3cf77c371dc49b91f24099f217329c76ce05&leagueId=766
+    //https://jsonkeeper.com/b/HWJ1
+
+    // final queryParametrs = {
+    //   'met': 'Teams',
+    //   'ApiKey':
+    //       '048b6af902a7611b4fd95f5ea44a3cf77c371dc49b91f24099f217329c76ce05',
+    //   'leagueId': '766'
+    // };
+
+    // final uri =
+    //     Uri.https('apiv2.allsportsapi.com', '/basketball/?', queryParametrs);
+
+    // final teamResponse =
+    //     await http.get(Uri.parse('https://jsonkeeper.com/b/HWJ1'),);
+
+    // List<TeamResponse> teamList = [];
+
+    // if (teamResponse.body == 200) {
+    //   var teamjson = json.decode(teamResponse.body);
+
+    //   for (var jsondata in teamjson) {
+    //     teamList.add(TeamResponse.fromJson(jsondata));
+    //   }
+    // }
+
+    // return teamList;
   }
 
   @override
   void initState() {
-    getTeam().then((value) {
-      setState(() {
-        _team.addAll(_team);
-      });
-    });
+    getTeam();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: _team.length,
-        itemBuilder: (BuildContext context, index) {
-          return Card(
-            child: Text(_team[index]['team_name'].toString()),
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder<Result>(
+              future: getTeam(),
+              builder: ((context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text('Loading');
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data?.teamName?.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Text(
+                            snapshot.data.teamName.toString(),
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              }),
+            ),
+          ),
+        ],
       ),
     );
   }
